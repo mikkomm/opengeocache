4c5db3f6cebeeb983ce16d76869a4900b18f5746
Mikko Majander <mikko.majander@iki.fi>
2014-02-03 19:26:21 +0200

    Remove unnecessary form

--- opengeocache/geocache/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import Modelform
-from geocache.models import Geocache
-
-class GeocacheForm(ModelForm):
-	class Meta:
-		model = Geocache
-		fields = ['cache_name', 'cache_description', 'cache_url', 'cache_secret']
