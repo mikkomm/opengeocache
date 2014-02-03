26c7eb70f743b1850668bb41077384ee030f1413
Mikko Majander <mikko.majander@iki.fi>
2014-02-03 18:56:44 +0200

    Added user as FK for geocache

--- opengeocache/geocache/models.py
+++ opengeocache/geocache/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Geocache(models.Model):
     cache_name          =   models.CharField('Cache name', max_length=256)
     cache_description   =   models.CharField('Cache description', max_length=1024)
     cache_url           =   models.CharField('Openstreetmap URL', max_length=128)
     cache_secret        =   models.CharField('Cache secret', max_length=128)
-
+    owner               =   models.ForeignKey(User)
 
     status_choices      =   (
             ('active','Active'),
