2874a1612ca0fd016c13c9fc66302feaf7280b8c
Mikko Majander <mikko.majander@iki.fi>
2014-02-03 19:25:59 +0200

    Capture the cache owner frmo request.

--- opengeocache/geocache/views.py
+++ opengeocache/geocache/views.py
@@ -13,4 +13,8 @@ class IndexView(generic.ListView):
 class GeocacheCreateView(CreateView):
 	model = Geocache
 	success_url = '/geocache/'
-	
+	fields = ['cache_name', 'cache_description', 'cache_type', 'cache_' 'cache_url', 'cache_secret']
+
+	def form_valid(self, form):
+		form.instance.owner = self.request.user
+		return super(GeocacheCreateView, self).form_valid(form)
