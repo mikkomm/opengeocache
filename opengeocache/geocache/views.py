bba43b5f13611af8811e04368e2b38f3ab9ef48c
Mikko Majander <mikko.majander@iki.fi>
2014-02-16 17:29:12 +0200

    Botched previous commit comment. Main thing was that login/logout was added and also mixin class to be used when checking if user has login.

--- opengeocache/geocache/views.py
+++ opengeocache/geocache/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import get_object_or_404, render
 from geocache.models import Geocache
 from django.views import generic
 from django.views.generic.edit import CreateView
+from django.contrib.auth import logout, authenticate, login
+
+from django.http import HttpResponseRedirect
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+class LoggedInMixin(object):
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(LoggedInMixin, self).dispatch(*args, **kwargs)
 
 class IndexView(generic.ListView):
 	template_name = 'geocache/index.html'
@@ -10,7 +21,7 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 		return Geocache.objects.all().order_by('-cache_name')
 
-class GeocacheCreateView(CreateView):
+class GeocacheCreateView(LoggedInMixin, CreateView):
 	model = Geocache
 	success_url = '/geocache/'
 	fields = ['cache_name', 'cache_description', 'cache_type', 'cache_url', 'cache_secret']
--- opengeocache/opengeocache/settings.py
+++ opengeocache/opengeocache/settings.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Login and logout
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/geocache/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -71,6 +76,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+TEMPLATE_DIRS = (
+    'opengeocache/templates',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
